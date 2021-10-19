3d2ac24016d65a7e5c9a6e94c41625fad311621f
va3wam <va3wam@gmail.com>
2021-10-19 18:39:11 -0400

    Swapped back and front switch pins.

--- include/zippy_gpio_pins.h
+++ include/zippy_gpio_pins.h
@@ -7,8 +7,8 @@
 
 #define _ZIPPITWIPI_GPIO_PINS_H // Preprocessor variable used by above check
 
-#define frontLimitSwitch PIN_LBL_32 // Front limit switch, physical pin 32 
-#define backLimitSwitch PIN_LBL_14 // Back limit switch, physical pin 14 
+#define backLimitSwitch PIN_LBL_14 // Back limit switch, physical pin 20 
+#define frontLimitSwitch PIN_LBL_32 // Front limit switch, physical pin 19 
 #define I2C_BUS0_SDA PIN_LBL_SDA // Serial Data Line (SDA) for I2C bus 0, physical pin 17
 #define I2C_BUS0_SCL PIN_LBL_SCL // Serial Clock Line (SCL) for I2C bus 0, physical pin 18
 #define I2C_BUS1_SDA PIN_LBL_TX // Serial Data Line (SDA) for I2C bus 1, physical pin 15
