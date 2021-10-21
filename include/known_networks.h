eaf450d437d199cf19173f6672dfe8c9d68be780
va3wam <va3wam@gmail.com>
2021-10-20 21:31:27 -0400

    Fixed variable names

--- include/known_networks.h
+++ include/known_networks.h
@@ -5,8 +5,8 @@
 
 #define amKnownNetworks_h
 
-static const int numKnownAPs = 5; // Number of known APs that the Robot knows how to connect to
-const String SSID[numKnownAPs] = { "MN_LIVINGROOM", "MN_WORKSHOP_2.4GHz", "MN_DS_OFFICE_2.4GHz", "MN_OUTSIDE", "borfpiggle"};
-const String Password[numKnownAPs] = { "5194741299", "5194741299", "5194741299", "5194741299", "de15ab00be"};
+static const int NUM_KNOWN_APS = 5; // Number of known APs that the Robot knows how to connect to
+const String SSID[NUM_KNOWN_APS] = { "MN_LIVINGROOM", "MN_WORKSHOP_2.4GHz", "MN_DS_OFFICE_2.4GHz", "MN_OUTSIDE", "borfpiggle"};
+const String Password[NUM_KNOWN_APS] = { "5194741299", "5194741299", "5194741299", "5194741299", "de15ab00be"};
 
 #endif
