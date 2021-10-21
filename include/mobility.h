5edcaf92915c80560679be8150bd81b139abbda2
va3wam <va3wam@gmail.com>
2021-10-21 15:31:28 -0400

    Ported MD25 functions over from libraries

--- include/mobility.h
+++ include/mobility.h
@@ -32,38 +32,113 @@ bool mobilityStatus = false;
 #define MD25CmdAutoTimeoutOff 0x32 // Disable automatic 2 second timeout of motors when no I2C activity
 #define MD25CmdAutoTimeoutOn 0x33 // Enable automatic 2 second timeout of motors when no I2C activitybool mobilityStatus = false; 
 
+// Define easily understood references to differentiate each motor and associated encoder.
+const bool LEFT_SIDE = 0; // Motor and encoder on left side of robot.
+const bool RIGHT_SIDE = 1; // Motor and encoder on right side of robot.
+
+/** 
+ * @brief Reads one byte from the MD25 regster via I2C.
+ * @param The register to be read.
+ * @return The value stored in the specified register. 
+=================================================================================================== */
+byte readRegisterByte(byte reg)
+{
+   Wire.beginTransmission(md25I2cAddress); // Request token to transmit on I2C bus
+   Wire.write(reg); // Send byte to read software version as a single byte
+   Wire.endTransmission(); // End request for token
+   Wire.requestFrom(md25I2cAddress, 1); // Request 1 byte from MD25 address register
+   while(Wire.available() < 1); // Wait for reply to request for address to arrive
+   return(Wire.read()); // Return address to   
+} // readRegisterByte
+
+/** 
+ * @brief Reads one byte from the MD25 regster via I2C.
+ * @param reg specifies which register to be read.
+ * @return The value stored in the specified register. 
+=================================================================================================== */
+long readEncoderArray(byte reg)
+{
+   Wire.beginTransmission(md25I2cAddress); // Get the attention of the MD25 controller.
+   Wire.write(reg); // Tell the controller which encoder we wish to read.
+   Wire.endTransmission(); // Release control of the I2C bus.
+   Wire.requestFrom(md25I2cAddress, 4); // Request 4 bytes from MD25
+   while(Wire.available() < 4); // Wait for 4 bytes to arrive
+   long position = Wire.read(); // First byte for encoder 1, HH.
+   position <<= 8;
+   position += Wire.read(); // Second byte for encoder 1, HL
+   position <<= 8;
+   position += Wire.read(); // Third byte for encoder 1, LH
+   position <<= 8;
+   position +=Wire.read(); // Fourth byte for encoder 1, LL
+   return(position);
+} // readEncoderArray()
+
+/** 
+ * @brief Get current reading of the specified motor encoder.
+ * @param reg specifies if the left or right encoder is to be read.
+ * @return value of the specified encoder. 
+=================================================================================================== */
+long getEncoder(bool reg)
+{                                            
+   if(reg == LEFT_SIDE) 
+   {
+      return(readEncoderArray(MD25RegEncoder1a)); // Return encoder reading for left motor.
+   } // if
+   else
+   {
+      return(readEncoderArray(MD25RegEncoder2a)); // Return encoder reading for right motor.
+   } // else
+} // getEncoder()
+
 /** 
  * @brief Reports the firmware version running on the MD25 motor controller. 
 =================================================================================================== */
 byte getMD25Version()
 {                                               
-   Log.verboseln("<amMD25::getFirmwareVersion> Retrieving firmware version from MD25");
-   Wire.beginTransmission(md25I2cAddress); // Request token to transmit on I2C bus
-   Wire.write(MD25RegSoftwareRev); // Send byte to read software version as a single byte
-   Wire.endTransmission(); // End request for token
-   Wire.requestFrom(md25I2cAddress, 1); // Request 1 byte from MD25 address register
-   while(Wire.available() < 1); // Wait for reply to request for address to arrive
-   byte software = Wire.read(); // Read it in
-   return(software); // Return address to 
-} //getMD25FirmwareVersion()
+   return(readRegisterByte(MD25RegSoftwareRev));
+} //getMD25Version()
 
 /** 
- * @brief Function to spin one or both motors at specified speed
- * @param motorNumber 0=left, 1=right, 2=both
- * @param speed How many encoder ticks to move
+ * @brief Reset both motor encoder values to 0.
+=================================================================================================== */
+void resetEncoder()
+{                                       
+  Wire.beginTransmission(md25I2cAddress); // Start conversation with motor controller
+  Wire.write(MD25RegCmd); // Set up command register
+  Wire.write(MD25CmdResetEncoders); // Send command to reset motor encoders
+  Wire.endTransmission(); 
+} //encodeReset()
+
+void setMode(int8_t mode)
+{
+   String explanation[4] = {"0 (full reverse) 128 (stop) 255 (full forward)",
+                            "-128 (full reverse), 0 (Stop), 127 (full forward)",
+                            "Speed1 controls both motors speed. Speed2 becomes the turn value. 0 (full reverse), 128 (stop), 255 (full forward)",
+                            "Speed1 control both motors speed, and Speed2 becomes the turn value. -128 (full reverse), 0 (stop), 127 (full forward)."};
+   Log.verboseln("<setMode> Setting mode to %d. Now Speed registers mean: %s", mode, explanation[mode].c_str());
+   Wire.beginTransmission(md25I2cAddress); // Request start transmit to the MD25 H-bridge.
+   Wire.write(MD25RegMode); // Going to write to the mode register.
+   Wire.write(mode); // Set mode.
+   Wire.endTransmission(); // End transmit.
+} // setMode
+
+/** 
+ * @brief Spin one or both motors at specified speed.
+ * @details This is a blocking function which will halt execution of the main logic until the 
+ * motor reaches it's end point. 
+ * @param motorNumber 0=left, 1=right, 2=both.
+ * @param speed How many encoder ticks to move.
+ * @param distance how far the robot should move.
  * @note See full detais at https://www.pishrobot.com/files/products/datasheets/md25.pdf
 =================================================================================================== */
-void spinMotor(int motorNumber, int speed)
+void spinMotor(int8_t motorNumber, int8_t speed, long distance)
 {
-  switch(motorNumber)
-  {
+   resetEncoder(); // Set both encoders to 0.
+   switch(motorNumber)
+   {
       case 0: // Left motor 
          Log.verboseln("<spinMotor> Spin left motor");
-         Wire.beginTransmission(md25I2cAddress); // Request start transmit to the MD25 H-bridge
-         Wire.write(MD25RegMode); // Going to write to the mode register
-         Wire.write(0); // Writing 0 sets speed controls to 0 (Full Reverse), 128 (Stop), 255 (Full Forward)
-         Wire.endTransmission(); // End transmit
-   //      delay(1); // Give bus a breather
+         setMode(0); // Set MD25 controller to mode 0.
          Wire.beginTransmission(md25I2cAddress); // Request start transmit to the MD25 H-bridge
          Wire.write(MD25RegSpeed1); // Indicate motor1 speed register
          Wire.write(speed); // 1-127 = backwards, 128 = stop, 129-255 = forward                                           
@@ -71,11 +146,7 @@ void spinMotor(int motorNumber, int speed)
          break;
       case 1: // Right motor 
          Log.verboseln("<spinMotor> Spin right motor");
-         Wire.beginTransmission(md25I2cAddress); // Request start transmit to the MD25 H-bridge
-         Wire.write(MD25RegMode); // Going to write to the mode register
-         Wire.write(0); // Writing 0 sets speed controls to 0 (Full Reverse), 128 (Stop), 255 (Full Forward)
-         Wire.endTransmission(); // End transmit
-   //      delay(1); // Give bus a breather
+         setMode(0); // Set MD25 controller to mode 0.
          Wire.beginTransmission(md25I2cAddress); // Request start transmit to the MD25 H-bridge
          Wire.write(MD25RegSpeed2); // Indicate motor1 speed register
          Wire.write(speed); // 1-127 = backwards, 128 = stop, 129-255 = forward                                           
@@ -83,17 +154,27 @@ void spinMotor(int motorNumber, int speed)
          break;
       default: // Both motors
          Log.verboseln("<spinMotor> Spin both motors");
-         Wire.beginTransmission(md25I2cAddress); // Request start transmit to the MD25 H-bridge
-         Wire.write(MD25RegMode); // Going to write to the mode register
-         Wire.write(2); // Writing 2 to the mode register will make speed1 control both motors speed
-         Wire.endTransmission(); // End transmit
-   //      delay(1); // Give bus a breather
+         setMode(2); // Set MD25 controller to mode 2.
          Wire.beginTransmission(md25I2cAddress); // Request start transmit to the MD25 H-bridge
          Wire.write(MD25RegSpeed1); // Indicate motor1 speed register
          Wire.write(speed); // 1-127 = backwards, 128 = stop, 129-255 = forward                                           
          Wire.endTransmission(); // End transmit
          break;
-  } // switch
+   } // switch
+   Log.verboseln("<initMobility> Readings before: Left encoder = %l, right encoder = %l", getEncoder(LEFT_SIDE), getEncoder(RIGHT_SIDE));
+   Log.verboseln("<initMobility> Wait for motor to  stop spinning.");
+   while(getEncoder(LEFT_SIDE) < distance)
+   {
+      Log.verboseln("<spinMotor> ... Still spinning.");
+      delay(10); // Give motor time to before taking next reading.
+   } // while   
+   // Stop both motors
+   Wire.beginTransmission(md25I2cAddress); // Request start transmit to the MD25 H-bridge
+   Wire.write(MD25RegSpeed1); // Indicate motor1 speed register
+   Wire.write(128); // 1-127 = backwards, 128 = stop, 129-255 = forward                                           
+   Wire.endTransmission(); // End transmit
+   delay(100); // Give motor time to stop.
+   Log.verboseln("<initMobility> Readings after: Left encoder = %l, right encoder = %l", getEncoder(LEFT_SIDE), getEncoder(RIGHT_SIDE));
 } // spinMotor()
 
 /**
@@ -105,7 +186,10 @@ bool initMobility()
    bool mStatus = true; // Status of mobility platform.
    Log.traceln("<initMobility> Initialize the drive train for this platform.");
    Log.verboseln("<initMobility> MD25 driver firmware version = %d", getMD25Version());
-   spinMotor(2,255);
+   const int8_t BOTH_MOTORS = 2;
+   int8_t speed = 180; // 0 - 127 backwards, 128 stop, 129 - 255 forward.
+   long distance = 100; // Distance to travel in millimeters.
+   spinMotor(BOTH_MOTORS, speed, distance); // Spin both motors.
    return mStatus;
 } // initMobility. 
 
